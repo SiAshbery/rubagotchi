5584b5c8fef8aa52f3becbb085dc232f32dd7e02
Simon Ashbery <sashbery@ASH-MAC.local>
2017-12-07 16:47:09 +0000

    Rubagotchis can get hungry

--- lib/rubagotchi.rb
+++ lib/rubagotchi.rb
@@ -1,8 +1,17 @@
 class Rubagotchi
     attr_accessor :name
-    
+
     def initialize(name)
         @name = name
+        @hunger = 0
+    end
+
+    def is_hungry?
+        @hunger >= 1
+    end
+
+    def add_hunger(amount)
+        @hunger += amount
     end
 
 end
--- spec/rubagotchi_spec.rb
+++ spec/rubagotchi_spec.rb
@@ -8,6 +8,17 @@ describe Rubagotchi do
         it 'Is created with a name' do
             expect(rubagotchi.name).to eq('Snuffles')
         end
+
+        it 'Starts life with a full belly' do
+            expect(rubagotchi.is_hungry?).to eq(false)
+        end
+    end
+
+    describe '#hunger' do
+        it 'Can get hungry' do
+            rubagotchi.add_hunger(1)
+            expect(rubagotchi.is_hungry?).to eq(true)
+        end
     end
 
 end
