aa226b8fad27e4ab4f495ca58d838c3ecf8e535f
siashbery <si.ashbery@gmail.com>
2017-12-10 13:53:38 +0000

    Game manager calls render_main_menu from user interface on initialize

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -5,6 +5,7 @@ class GameManager
 
   def initialize(user_interface = UserInterface.new)
     @user_interface = user_interface
+    @user_interface.render_main_menu
   end
 
 end
--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -8,11 +8,17 @@ describe GameManager do
 
     before(:each) do
       @user_interface = double(:user_interface)
+      allow(@user_interface).to receive(:render_main_menu)
+
       @game_manager = GameManager.new(@user_interface)
     end
 
     it 'Initializes with a user interface' do
       expect(@game_manager.user_interface).to eq(@user_interface)
+    end
+
+    it 'Calls render main menu on user interface' do
+      expect(@user_interface).to have_received(:render_main_menu)
     end
 
   end
