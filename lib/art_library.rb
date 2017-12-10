09cabd22830110a427c9dbdc757c72a4b1df3b05
siashbery <si.ashbery@gmail.com>
2017-12-10 23:41:46 +0000

    Creates art library class with rubagotchi idle pose ascii asset

---
+++ lib/art_library.rb
@@ -0,0 +1,45 @@
+class ArtLibrary
+
+  def rubagotchi_idle_pose
+    "                                     XXXXXX\n"\
+    "                                   XXXXXXXXX\n"\
+    "                                   XXXXXXXXXX\n"\
+    "                                   XXXXX XXXX\n"\
+    "                                   XXXXXXXXX\n"\
+    "                                     X XXXX\n"\
+    "                                        X\n"\
+    "                                         X\n"\
+    "                                        X\n"\
+    "                                       X\n"\
+    "                                        X\n"\
+    "                                       X\n"\
+    "                                     XX\n"\
+    "                                 X X   X X\n"\
+    "                          X X XX            XXX\n"\
+    "                        XXX                   XXXXX\n"\
+    "                     XXd                           XXXX\n"\
+    "                   XX                                  XX\n"\
+    "                  XX                                      XX\n"\
+    "                XX                                        XX\n"\
+    "               XX      XXXXXX             XXXXXXX          XXX\n"\
+    "             X      XXXX   XXXX        XXXX    XXX          XX\n"\
+    "            XX     XXXXXX   XXX       XXXXXX   XXXX           X\n"\
+    "           X       XXXXXXXXXXXX       XXXXXXXXXXXXX           X\n"\
+    "           X       XXXXXXXXXXXX       XXXXXXXXXXXX            X\n"\
+    "           X         XXXXXXXXX          XXXXXXXXX             X\n"\
+    "           X                                                 X\n"\
+    "           X                                                 X\n"\
+    "           XX                                                X\n"\
+    "           X                                               XX\n"\
+    "            XX            X       X           X              XX\n"\
+    "             X            XXX    XXX         XX              XXXXX\n"\
+    "            XXXX              XXXXXX XXXXXXXXX               XX  XXXXXXX\n"\
+    "           XX  X                                              X          XX\n"\
+    "         XX                                                        XXXX\n"\
+    "       XXX                                                 XXXXXXX\n"\
+    "         XXXXXXX                                      XXXXXXX\n"\
+    "           XXXXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXX\n"\
+    "                         XXXXXXXXX\n"
+  end
+
+end
