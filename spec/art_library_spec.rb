d3a0758522d5060e749472ff2853f66fc1648355
Simon Ashbery <sashbery@ASH-MAC.local>
2017-12-18 13:58:54 +0000

    simplifies rubagotchi ascii art

--- spec/art_library_spec.rb
+++ spec/art_library_spec.rb
@@ -34,45 +34,11 @@ describe ArtLibrary do
 
     it 'Returns the idle pose for a rubagotchi' do
       expect(@art_library.rubagotchi_idle_pose).to eq(
-        "                                        XXXXXX\n"\
-        "                                      XXXXXXXXX\n"\
-        "                                      XXXXXXXXXX\n"\
-        "                                      XXXXX XXXX\n"\
-        "                                      XXXXXXXXX\n"\
-        "                                        X XXXX\n"\
-        "                                           X\n"\
-        "                                            X\n"\
-        "                                           X\n"\
-        "                                          X\n"\
-        "                                           X\n"\
-        "                                          X\n"\
-        "                                        XX\n"\
-        "                                    X X   X X\n"\
-        "                             X X XX            XXX\n"\
-        "                           XXX                   XXXXX\n"\
-        "                        XXd                           XXXX\n"\
-        "                      XX                                  XX\n"\
-        "                     XX                                      XX\n"\
-        "                   XX                                        XX\n"\
-        "                  XX      XXXXXX             XXXXXXX          XXX\n"\
-        "                X      XXXX   XXXX        XXXX    XXX          XX\n"\
-        "               XX     XXXXXX   XXX       XXXXXX   XXXX           X\n"\
-        "              X       XXXXXXXXXXXX       XXXXXXXXXXXXX           X\n"\
-        "              X       XXXXXXXXXXXX       XXXXXXXXXXXX            X\n"\
-        "              X         XXXXXXXXX          XXXXXXXXX             X\n"\
-        "              X                                                 X\n"\
-        "              X                                                 X\n"\
-        "              XX                                                X\n"\
-        "              X                                               XX\n"\
-        "               XX            X       X           X              XX\n"\
-        "                X            XXX    XXX         XX              XXXXX\n"\
-        "               XXXX              XXXXXX XXXXXXXXX               XX  XXXXXXX\n"\
-        "              XX  X                                              X          XX\n"\
-        "            XX                                                        XXXX\n"\
-        "          XXX                                                 XXXXXXX\n"\
-        "            XXXXXXX                                      XXXXXXX\n"\
-        "              XXXXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXX\n"\
-        "                            XXXXXXXXX\n"
+                 " |^|   |^|\n"\
+                 " : )   ( :\n"\
+                 "|   O O   |\n"\
+                 "(  )-v-(  )\n"\
+                 " [m_____m]\n"
       )
     end
 
